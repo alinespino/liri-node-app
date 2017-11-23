e6777870bb229492a819d30ef22f64fd053b96bb
Aline Espino <alin.e.spino@gmail.com>
2017-11-23 12:32:52 -0800

    added twitter keys

---
+++ keys.js
@@ -0,0 +1,12 @@
+
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'eU26Z05OrZV9BEEQAhGbiLxI4',
+  consumer_secret: 'JT2Ktg8dG6KpwImQZm9338OnNHExEJw5sfB0YKkbEuAXGnj3kE',
+  access_token_key: '39117384-NOd1ylaeJ41hQNYLS6HdH1HRoiQamXwhNQFHhkI4Q',
+  access_token_secret: 'idffZdVFnIuC6mT0klrOkh3e05NLLeMjJoravTR7oi3P1',
+}
+
+module.exports = twitterKeys;
+
