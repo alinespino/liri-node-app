1063b885c2770c2e9f84e1355c9b9d0698bf51f8
Aline Espino <alin.e.spino@gmail.com>
2017-11-29 23:47:01 -0800

    twitter

--- liri.js
+++ liri.js
@@ -1,6 +1,4 @@
 
-var keys = require ("./keys.js");
-
 
 // Commands //
 
@@ -10,25 +8,54 @@ var keys = require ("./keys.js");
 // movie-this
 // do-what-it-says
 
+//TWITTER // 
+
+var keys = require ("./keys.js");
+var Twitter = require('twitter');
+var client = keys;
+var myTweets = process.argv[2]
+
+
+if (myTweets === "my-tweets"){
+
+    var params = {
+        count:20
+    };
+
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+      if (!error) {
+        console.log(tweets);
+      }
+    });
+}
+
+// client.get('search/tweets', {q: 'node.js'}, function(error, tweets, response) {
+//     console.log(tweets);
+//  });
+
+
 
 // OMDB
 // node liri.js movie-this '<movie name here>'
 
 var request = require("request");
 var movie = process.argv[3];
+var omdb = process.argv[2]
+
 
 var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
 
 // debug // 
-console.log(queryUrl);
+// console.log(queryUrl);
 
 request(queryUrl, function(error, response, body) {
-    
-      // If the request is successful
+
+//user must type movie-this // 
+    if ( omdb === "movie-this") {
+
+ // If the request is successful//
       if (!error && response.statusCode === 200) {
     
-
-        // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
         console.log("Title of the movie:" + JSON.parse(body).Title);
         console.log("Released on: " + JSON.parse(body).Year);
         console.log("IMDB Rating:" + JSON.parse(body).imdbRating);
@@ -36,20 +63,34 @@ request(queryUrl, function(error, response, body) {
         console.log("Country:" + JSON.parse(body).Country);
         console.log("Plot:" + JSON.parse(body).Plot);
         console.log("Actors:" + JSON.parse(body).Actors);
-
       }
+    }
     });
 
-// * Title of the movie.
-// * Year the movie came out.
-// * IMDB Rating of the movie.
-// * Rotten Tomatoes Rating of the movie.
-// * Country where the movie was produced.
-// * Language of the movie.
-// * Plot of the movie.
-// * Actors in the movie.
+    // NEED TO SOLVE HOW TO CALL MR NOBODY // 
 
-// If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-// If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/
-// It's on Netflix!
-// You'll use the request package to retrieve data from the OMDB API. Like all of the in-class activities, the OMDB API requires an API key. You may use trilogy.
+// if (movie === " ") {
+//     var blank = "Mr.Nobody";
+//     var newQueryUrl = "http://www.omdbapi.com/?t=" + blank + "&y=&plot=short&apikey=trilogy";
+//     console.log(newQueryUrl);
+    
+//     request(newQueryUrl, function(error, response, body) {
+//         if (!error && response.statusCode === 200) {
+            
+//                 console.log("Title of the movie:" + JSON.parse(body).Title);
+//                 console.log("Released on: " + JSON.parse(body).Year);
+//                 console.log("IMDB Rating:" + JSON.parse(body).imdbRating);
+//                 console.log("Rotten Tomatoes Rating:" + JSON.parse(body).Ratings[1].Source);
+//                 console.log("Country:" + JSON.parse(body).Country);
+//                 console.log("Plot:" + JSON.parse(body).Plot);
+//                 console.log("Actors:" + JSON.parse(body).Actors);
+                //    console.log("If you haven't watched 'Mr. Nobody', then you should: http://www.imdb.com/title/tt0485947/"+
+                //     "It's on Netflix!");
+
+//               }
+            
+//             });
+// }
+
+
+
